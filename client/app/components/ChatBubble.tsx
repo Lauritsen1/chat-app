7ec4b049650ef02f00f04c8b7f56a643f8642e7c
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2023-06-21 17:04:28 +0200

    Update ChatBubble component for useridentification

--- client/app/components/ChatBubble.tsx
+++ client/app/components/ChatBubble.tsx
@@ -1,20 +1,23 @@
 import Image from 'next/image'
 
-import { Message } from '../lib/types'
+import { useUser } from '@clerk/nextjs'
 
-export default function ChatBubble(props: { message: Message }) {
-  const { id, name, content, time, imageUrl, isMine } = props.message
+import { MessageType } from '@/app/lib/types'
+
+export default function ChatBubble(props: { message: MessageType }) {
+  const { sender, content } = props.message
+
+  // User state
+  const { user } = useUser()
 
   return (
-    <div className={`chat ${isMine ? 'chat-end' : 'chat-start'}`}>
+    <div className={`chat ${sender === user?.id ? 'chat-end' : 'chat-start'}`}>
       <div className='chat-image avatar'>
-        <div className='w-10 rounded-full'>
-          <img src={imageUrl} />
-        </div>
+        <div className='w-10 rounded-full'>{/* <img src={imageUrl} /> */}</div>
       </div>
       <div
         className={`chat-bubble ${
-          isMine
+          sender === user?.id
             ? 'bg-primary text-primary-content'
             : 'bg-neutral text-neutral-content'
         }`}
